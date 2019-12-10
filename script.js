5cda01ef8924d38fca2b9e62aff6b54c84c41909
Andrea <anguerra@pdx.edu>
2019-12-10 01:33:27 -0800

    interactions

---
+++ script.js
@@ -0,0 +1,17 @@
+// your script file for adding your own jquery
+
+
+$(function () {
+    // Your Code from here on down. Don't delete that line above!
+
+$('.clickme').click(function(){
+		$('audio#chomp')[0].play()
+
+})
+
+
+
+
+
+
+});
